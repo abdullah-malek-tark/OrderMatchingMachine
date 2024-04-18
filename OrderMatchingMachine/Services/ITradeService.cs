5cbf07e3e01b08ace25881a49475fe06fc056a6d
abdullah-malek-tark <abdullah.malek@tarktech.com>
2024-04-18 18:24:07 +0530

    Removed redundant Code

--- OrderMatchingMachine/Services/ITradeService.cs
+++ OrderMatchingMachine/Services/ITradeService.cs
@@ -5,8 +5,5 @@ namespace OrderMatchingMachine.Services
     public interface ITradeService
     {
         public bool TryExecuteTradeForOrder(Order order, out Order updatedOrder);
-
-        //Trade MatchOrder(Order order);
-        //List<Trade> MatchOrders(List<Order> orders);
     }
 }
--- OrderMatchingMachine/Services/TradeService.cs
+++ OrderMatchingMachine/Services/TradeService.cs
@@ -33,13 +33,11 @@ namespace OrderMatchingMachine.Services
         {
             if(order.OrderType == OrderType.BID)
             {
-                var matchedOrder = _orderRepository.GetMatchingSellOrder(order).OrderBy(o => o.AddedAt).FirstOrDefault(o => o.Price <= order.Price);
-                return matchedOrder;
+                return _orderRepository.GetMatchingSellOrder(order).OrderBy(o => o.AddedAt).FirstOrDefault(o => o.Price <= order.Price);
             }
             else
             {
-                var matchedOrder = _orderRepository.GetMatchingBuyOrder(order).OrderBy(o => o.AddedAt).FirstOrDefault(o => o.Price >= order.Price);
-                return matchedOrder;
+                return _orderRepository.GetMatchingBuyOrder(order).OrderBy(o => o.AddedAt).FirstOrDefault(o => o.Price >= order.Price);
             }
         }
 
@@ -53,64 +51,5 @@ namespace OrderMatchingMachine.Services
 
             return trade;
         }
-
-        //private static readonly Dictionary<string, Order> _orderStore = new Dictionary<string, Order>();
-        //private static List<Order> _orders = new List<Order>();
-
-        //public Trade MatchOrder(Order order)
-        //{
-        //    if(order.OrderType == OrderType.BID)
-        //    {
-        //        var matchingSellOrder = _orders.OrderBy(o => o.AddedAt).FirstOrDefault(o => o.OrderType == OrderType.OFFER && o.Price <= order.Price && o.CompanyName.Equals(order.CompanyName));
-
-        //        if(matchingSellOrder != null)
-        //        {
-        //            _orders.RemoveAll(o => o.OrderId == matchingSellOrder.OrderId);
-        //            return new Trade
-        //            {
-        //                BuyerOrderId = order.OrderId,
-        //                SellerOrderId = matchingSellOrder.OrderId,
-        //                CompanyName = matchingSellOrder.CompanyName,
-        //                Price = matchingSellOrder.Price,
-        //            };
-        //        }
-        //    }
-        //    else
-        //    {
-        //        var buyOrder = _orders.OrderBy(o => o.AddedAt).FirstOrDefault(o => o.OrderType == OrderType.BID && o.Price >= order.Price && o.CompanyName.Equals(order.CompanyName));
-
-        //        if(buyOrder != null)
-        //        {
-        //            _orders.RemoveAll(o => o.OrderId == buyOrder.OrderId);
-        //            return new Trade
-        //            {
-        //                BuyerOrderId = buyOrder.OrderId,
-        //                SellerOrderId = order.OrderId,
-        //                CompanyName = buyOrder.CompanyName,
-        //                Price = buyOrder.Price,
-        //            };
-        //        }
-        //    }
-        //    _orders.Add(order);
-        //    DisplayPlacedOrders();
-        //    return null;
-        //}
-
-        //public List<Trade> MatchOrders(List<Order> orders)
-        //{
-        //    return orders.Select(MatchOrder)
-        //        .Where(trade => trade != null)
-        //        .ToList();
-        //}
-
-        //private void DisplayPlacedOrders()
-        //{
-        //    Console.WriteLine("------------------");
-        //    foreach (Order o in _orders)
-        //    {
-        //        Console.WriteLine($"{o.OrderId}, {o.TraderName}, {o.CompanyName}, {o.OrderType}, {o.Price}, {o.AddedAt}");
-        //    }
-        //    Console.WriteLine("------------------");
-        //}
     }
 }
